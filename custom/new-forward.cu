83041c93424b30f2964b8c58ff725dd14a24095b
Jimmy Zhang <jimmyjz2@exodus.csl.illinois.edu>
2020-12-16 02:03:04 -0600

    tensors working unoptimized

--- custom/new-forward.cu
+++ custom/new-forward.cu
@@ -1,15 +1,21 @@
-//#include <__clang_cuda_builtin_vars.h>
+
 #include <cmath>
 #include <iostream>
 #include "gpu-new-forward.h"
 #include "cuda_fp16.h"
+#include <mma.h>
 #define TILE_WIDTH 16 //fixed for tensors
 #define KERNEL_SIZE 7
+
+#define WMMA_M 16
+#define WMMA_N 16
+#define WMMA_K 16
 
 __constant__ float K_ONE[4*KERNEL_SIZE*KERNEL_SIZE];
 __constant__ float K_TWO[16*4*KERNEL_SIZE*KERNEL_SIZE];
 
 //C = A*B
+using namespace nvcuda;
 __global__ void matrixMultiplyShared(float *x, float *k, float *y,
                                      const int B, const int M, const int C, const int H, const int W, const int K) {
 
@@ -19,6 +25,7 @@ __global__ void matrixMultiplyShared(float *x, float *k, float *y,
     const int x_cols = H_out*W_out;
     const int k_rows = M; 
     const int k_cols = K*K*C;
+    const int b = blockIdx.z;
 
     #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
     #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
@@ -27,6 +34,10 @@ __global__ void matrixMultiplyShared(float *x, float *k, float *y,
 
     #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 
+    wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::row_major> a_frag;
+    wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::row_major> b_frag;
+    wmma::fragment<wmma::accumulator, 16, 16, 16, float> c_frag;
+    wmma::fill_fragment(c_frag, 0.0f);
 
 
     int row = blockIdx.y * TILE_WIDTH + threadIdx.y; 
@@ -34,59 +45,77 @@ __global__ void matrixMultiplyShared(float *x, float *k, float *y,
 
     __shared__ __half rowTile[TILE_WIDTH][TILE_WIDTH];
     __shared__ __half colTile[TILE_WIDTH][TILE_WIDTH];
-    
+    __shared__ float outTile[TILE_WIDTH][TILE_WIDTH];
 
-    for(int b=0; b<B; b++){
-        __half accum = 0;
+    const half *row_ptr = &rowTile[0][0];
+    const half *col_ptr = &colTile[0][0];
+    float *out_ptr = &outTile[0][0];
 
 
+    __half accum = 0;
+    for(int p=0; p<ceil(k_cols / (float)TILE_WIDTH); ++p){
 
-        for(int p=0; p<ceil(k_cols / (float)TILE_WIDTH); ++p){
+        int c = (threadIdx.x + p*TILE_WIDTH)/(K*K);
+        int k_remain =  (threadIdx.x + p*TILE_WIDTH) % (K*K);
+        if(M==4){
 
-            int c = (threadIdx.x + p*TILE_WIDTH)/(K*K);
-            int k_remain =  (threadIdx.x + p*TILE_WIDTH) % (K*K);
-            if(M==4){
-
-                if (row < k_rows && (threadIdx.x + p*TILE_WIDTH) < k_cols) 
-                    rowTile[threadIdx.y][threadIdx.x] = __float2half(k4d(row, c, k_remain/K, k_remain%K));
-                else
-                    rowTile[threadIdx.y][threadIdx.x] = 0.0;
-                }
-            else{
-                if (row < k_rows && (threadIdx.x + p*TILE_WIDTH) < k_cols) 
-                    rowTile[threadIdx.y][threadIdx.x] = __float2half(k4d(row, c, k_remain/K, k_remain%K));
-                else
-                    rowTile[threadIdx.y][threadIdx.x] = 0.0;
-            }
-            
-            c = (threadIdx.y + p*TILE_WIDTH)/(K*K);
-            int x_remain =  (threadIdx.y + p*TILE_WIDTH) % (K*K);
-            int y_in = col/W_out;
-            int x_in = col%W_out;
-
-            if (col < x_cols && (threadIdx.y+TILE_WIDTH*p) < x_rows)
-                //colTile[threadIdx.y][threadIdx.x] = x_unroll_4d(b, (threadIdx.y + p*TILE_WIDTH), col);
-                colTile[threadIdx.y][threadIdx.x] = __float2half(x4d(b,c, y_in + x_remain/K, x_in + x_remain%K));
+            if (row < k_rows && (threadIdx.x + p*TILE_WIDTH) < k_cols) 
+                rowTile[threadIdx.y][threadIdx.x] = __float2half(k4d(row, c, k_remain/K, k_remain%K));
             else
-                colTile[threadIdx.y][threadIdx.x] = 0.0;
-
-            __syncthreads();
-
-            for(int i=0; i<TILE_WIDTH; ++i){
-                accum += rowTile[threadIdx.y][i] * colTile[i][threadIdx.x];
+                rowTile[threadIdx.y][threadIdx.x] = 0.0;
             }
-            __syncthreads();
-
+        else{
+            if (row < k_rows && (threadIdx.x + p*TILE_WIDTH) < k_cols) 
+                rowTile[threadIdx.y][threadIdx.x] = __float2half(k4d(row, c, k_remain/K, k_remain%K));
+            else
+                rowTile[threadIdx.y][threadIdx.x] = 0.0;
         }
-        if (row < M && col < H_out*W_out) 
-            y4d(b, row, col/W_out, col%W_out) = __half2float(accum);
+        
+        c = (threadIdx.y + p*TILE_WIDTH)/(K*K);
+        int x_remain =  (threadIdx.y + p*TILE_WIDTH) % (K*K);
+        int y_in = col/W_out;
+        int x_in = col%W_out;
+
+        if (col < x_cols && (threadIdx.y+TILE_WIDTH*p) < x_rows)
+            colTile[threadIdx.y][threadIdx.x] = __float2half(x4d(b,c, y_in + x_remain/K, x_in + x_remain%K));
+        else
+            colTile[threadIdx.y][threadIdx.x] = 0.0;
+
+        __syncthreads();
+
+        wmma::load_matrix_sync(a_frag, row_ptr, 16);
+        wmma::load_matrix_sync(b_frag, col_ptr, 16);
+        
+        wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+
+
+        // for(int i=0; i<TILE_WIDTH; ++i){
+        //     accum += rowTile[threadIdx.y][i] * colTile[i][threadIdx.x];
+        // }
+        //__syncthreads();
+
     }
+    wmma::store_matrix_sync(out_ptr, c_frag, 16, wmma::mem_row_major);
+
+    // if (blockIdx.x == 200  && blockIdx.y == 0  && blockIdx.z == 0 && M==4 && threadIdx.x < 3){
+    //         printf("otm[%d][%d] = %f\n", threadIdx.y, threadIdx.x, outTile[threadIdx.y][threadIdx.x]);
+    //         printf("tid[%d][%d] = %f\n", threadIdx.y, threadIdx.x, __half2float(accum));
+
+    // }
+    // if (blockIdx.x == 0 && blockIdx.y == 0 && blockIdx.z == 0){
+    //     printf("tid[%d][%d] = %f\n", threadIdx.x, threadIdx.y, __half2float(accum));
+    // }
+
+    if (row < M && col < H_out*W_out) {
+        y4d(b, row, col/W_out, col%W_out) = outTile[threadIdx.y][threadIdx.x];
+    }
+            //y4d(b, row, col/W_out, col%W_out) = outTile[threadIdx.y][threadIdx.x];
+
   
 }
 
 __host__ void GPUInterface::conv_forward_gpu(float *host_y, const float *host_x, const float *host_k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
-    int B_interval = (B>5000)?10000:B;
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
     // Declare relevant device pointers
@@ -94,13 +123,12 @@ __host__ void GPUInterface::conv_forward_gpu(float *host_y, const float *host_x,
     float *device_x;
     float *device_k;
 
-
-     std::cout << "K: " << K << " M: " << M << " C: " << C << std::endl;
-     std::cout << "B: " << B << " H: " << H << " W: " << W << std::endl;
-     std::cout << "H_out: " << H_out << " W_out: " << W_out << std::endl;
+    //  std::cout << "K: " << K << " M: " << M << " C: " << C << std::endl;
+    //  std::cout << "B: " << B << " H: " << H << " W: " << W << std::endl;
+    //  std::cout << "H_out: " << H_out << " W_out: " << W_out << std::endl;
 
     // Allocate memory and copy over the relevant data structures to the GPU
-    cudaMalloc((void **) &device_x, (B_interval*C*H*W) * sizeof(float));
+    cudaMalloc((void **) &device_x, (B*C*H*W) * sizeof(float));
     cudaMalloc((void **) &device_k, (C*M*K*K) * sizeof(float));
     cudaMalloc((void **) &device_y, (B*M*H_out*W_out) * sizeof(float));
 
@@ -116,15 +144,15 @@ __host__ void GPUInterface::conv_forward_gpu(float *host_y, const float *host_x,
     if (M == 16) cudaMemcpyToSymbol(K_TWO, device_k, M*C*K*K*sizeof(float));
 
                       
-    for (int b = 0; b<B; b+=B_interval){
-        val = cudaMemcpy(device_x, host_x + b*C*H*W, (B_interval*C*H*W) * sizeof(float), cudaMemcpyHostToDevice);
 
-        dim3 gridDim(ceil(H_out*W_out/(1.0 * TILE_WIDTH)), ceil(M/(1.0 * TILE_WIDTH)), 1);
-        dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
-        matrixMultiplyShared<<< gridDim, blockDim>>>(device_x, device_k, device_y,
-                    B_interval, M, C, H, W, K);
-        val = cudaMemcpy(host_y + b*M*H_out*W_out, device_y, (B_interval*M*H_out*W_out) * sizeof(float), cudaMemcpyDeviceToHost);
-    }
+    val = cudaMemcpy(device_x, host_x, (B*C*H*W) * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 gridDim(ceil(H_out*W_out/(1.0 * TILE_WIDTH)), ceil(M/(1.0 * TILE_WIDTH)), B);
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
+    matrixMultiplyShared<<< gridDim, blockDim>>>(device_x, device_k, device_y,
+                B, M, C, H, W, K);
+    val = cudaMemcpy(host_y, device_y, (B*M*H_out*W_out) * sizeof(float), cudaMemcpyDeviceToHost);
+    
 
     cudaError_t error = cudaGetLastError();
     if(error != cudaSuccess)
